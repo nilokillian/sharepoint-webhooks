cf6fc4752e8af50d8a7991b679bc688961a5e6d4
nilokillian <nilokillian@gmail.com>
2019-08-09 17:53:25 +1200

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/_api", (req, res) => {
+  res.send("Works");
+});
+
+app.get("/", (req, res) => {
+  res.send("Works root");
+});
+
+app.post("/webHook", (req, res) => {
+  const msg = processRequest(req);
+
+  res.send(msg);
+});
+
+function processRequest(req) {
+  console.log(
+    `Incoming request for ${req.protocol}://${req.get("host")} ${
+      req.orogonalUrl
+    }`
+  );
+
+  if (req.query.validationToken !== null) {
+    console.log(
+      `Subscription confirmed with Token : ${req.query.validationToken}`
+    );
+    return req.query.validationToken;
+  }
+}
+const port = 3009;
+app.listen(port, () => console.log(`port ${port} is being listened`));
